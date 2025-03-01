5e2850d9486b7f81c07dc640a68fde32b48bdc91
BabyGirlCodes <d0272896@student.dkit.ie>
2025-03-01 13:25:12 +0000

    Refix: Added Java documentation to Song Class

--- Part2_LinkedLists/src/main/java/business/Song.java
+++ Part2_LinkedLists/src/main/java/business/Song.java
@@ -8,25 +8,49 @@ public class Song {
     private String artist;
     private String title;
 
+    /**
+     * Constructor setting the artist and title to a default value of "Joe Bloggs" and "Mmmbops"
+     */
     public Song(){
         this.artist = "Joe Bloggs";
         this.title = "Mmmbops";
     }
 
+    /**
+     * Construct a song object to a specified artist or title
+     * @param artist  The name of the artist
+     * @param title The title of the song
+     */
     public Song(String artist, String title){
         this.artist = artist;
         this.title = title;
     }
+
+    /**
+     * @return the name of the artist
+     */
 //Getters and Setters
     public String getArtist() {
         return artist;
     }
+
+    /**
+     * Set a new artist name
+     */
     public void setArtist(){
         this.artist = artist;
     }
+
+    /**
+     * @return The title of the song
+     */
     public String getTitle(){
         return title;
     }
+
+    /**
+     * Set a new title of the song
+     */
     public void setTitle(){
         this.title = title;
     }
