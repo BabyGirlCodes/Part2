a4a945da868e69d09191c9de8db650e2a902554f
BabyGirlCodes <d0272896@student.dkit.ie>
2025-03-02 11:24:07 +0000

    Feature: Introducing a basic LinkedList class. It has a field for the first node, last node and number of elements . A constructor is present to set the first node to null and the number of elements or size of the list to 0.

--- Part2_LinkedLists/src/main/java/utils/LinkedList.java
+++ Part2_LinkedLists/src/main/java/utils/LinkedList.java
@@ -1,9 +1,33 @@
 package utils;
 
+
+
 /**
  *
  * @author michelle
  */
-public class LinkedList {
-    
+public class LinkedList
+{
+    private Node first;
+    private Node last;
+    private int numElements;
+
+    public LinkedList() {
+        this.first = null;
+        this.numElements = 0;
+    }
+
+    private static class Node {
+        private String data;
+        private Node next;
+
+
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+
 }
